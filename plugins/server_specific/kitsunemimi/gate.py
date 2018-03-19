d41d9211698dfe75e610849b6c6d063eb2afaff2
awakening <awaken1ng@users.noreply.github.com>
2018-03-19 15:19:12 +0700

    Add on start check for /r/kitsunemimi gate

--- plugins/server_specific/kitsunemimi/gate.py
+++ plugins/server_specific/kitsunemimi/gate.py
@@ -7,11 +7,25 @@ import asyncio
 class Plugin(ServerPlugin):
     name = '/r/kitsunemimi gate'
 
-    async def on_member_join(self, member: discord.Member):
-        delay = 300
-        regular_floof = utils.find(lambda r: r.id == 398977628389900288, member.guild.roles)
+    delay = 300
+    autorole_id = 398977628389900288
 
-        await asyncio.sleep(delay)
+    async def on_ready(self):
+        guild = self.bot.get_guild(*self.servers)
+        regular_floof = utils.find(lambda r: r.id == self.autorole_id, guild.roles)
+        no_roles = [member
+                    for member in guild.members
+                    if len(member.roles) == 1]
+        if no_roles:
+            await asyncio.sleep(self.delay)
+            for member in no_roles:
+                if member in guild.members and regular_floof not in member.roles:
+                    await member.add_roles(regular_floof, reason='Autorole on_ready check')
+
+    async def on_member_join(self, member: discord.Member):
+        regular_floof = utils.find(lambda r: r.id == self.autorole_id, member.guild.roles)
+
+        await asyncio.sleep(self.delay)
         if member not in member.guild.members:
             return
-        await member.add_roles(regular_floof)
+        await member.add_roles(regular_floof, reason='Autorole')
