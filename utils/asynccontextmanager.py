ce5337ef5ba8bf47b680a5670eeae3fc50aa5975
awakening <awaken1ng@users.noreply.github.com>
2018-03-20 22:42:01 +0700

    Fix asynccontextmanager decorator to execute code after yielding the value

--- utils/asynccontextmanager.py
+++ utils/asynccontextmanager.py
@@ -1,21 +1,18 @@
 from contextlib import wraps
 import abc
-# import sys
+import sys
 import _collections_abc
 
-""" Slight modification of contextlib.contextmanager """
+
+"""Modification of contextlib.contextmanager"""
 
 
 class AbstractAsyncContextManager(abc.ABC):
-    """An abstract base class for context managers."""
-
     async def __aenter__(self):
-        """Return `self` upon entering the runtime context."""
         return self
 
     @abc.abstractmethod
     async def __aexit__(self, exc_type, exc_value, traceback):
-        """Raise any exception triggered within the runtime context."""
         return None
 
     @classmethod
@@ -26,18 +23,7 @@ class AbstractAsyncContextManager(abc.ABC):
 
 
 class AsyncContextDecorator(object):
-    "A base class or mixin that enables context managers to work as decorators."
-
     def _recreate_cm(self):
-        """Return a recreated instance of self.
-
-        Allows an otherwise one-shot context manager like
-        _GeneratorContextManager to support use as
-        a decorator via implicit recreation.
-
-        This is a private interface just for _GeneratorContextManager.
-        See issue #11647 for details.
-        """
         return self
 
     def __call__(self, func):
@@ -50,26 +36,15 @@ class AsyncContextDecorator(object):
 
 
 class _AsyncGeneratorContextManager(AsyncContextDecorator, AbstractAsyncContextManager):
-    """Helper for @contextmanager decorator."""
-
     def __init__(self, func, args, kwds):
         self.gen = func(*args, **kwds)
         self.func, self.args, self.kwds = func, args, kwds
-        # Issue 19330: ensure context manager instances have good docstrings
         doc = getattr(func, "__doc__", None)
         if doc is None:
             doc = type(self).__doc__
         self.__doc__ = doc
-        # Unfortunately, this still doesn't provide good help output when
-        # inspecting the created context manager instances, since pydoc
-        # currently bypasses the instance docstring and shows the docstring
-        # for the class instead.
-        # See http://bugs.python.org/issue19404 for more details.
 
     def _recreate_cm(self):
-        # _GCM instances are one-shot context managers, so the
-        # CM must be recreated each time a decorated function is
-        # called
         return self.__class__(self.func, self.args, self.kwds)
 
     async def __aenter__(self):
@@ -80,51 +55,34 @@ class _AsyncGeneratorContextManager(AsyncContextDecorator, AbstractAsyncContextM
 
     async def __aexit__(self, type, value, traceback):
         pass
-        # if type is None:
-        #     try:
-        #         next(self.gen)
-        #     except StopIteration:
-        #         return False
-        #     else:
-        #         raise RuntimeError("generator didn't stop")
-        # else:
-        #     if value is None:
-        #         # Need to force instantiation so we can reliably
-        #         # tell if we get the same exception back
-        #         value = type()
-        #     try:
-        #         self.gen.athrow(type, value, traceback)
-        #     except StopIteration as exc:
-        #         # Suppress StopIteration *unless* it's the same exception that
-        #         # was passed to throw().  This prevents a StopIteration
-        #         # raised inside the "with" statement from being suppressed.
-        #         return exc is not value
-        #     except RuntimeError as exc:
-        #         # Don't re-raise the passed in exception. (issue27122)
-        #         if exc is value:
-        #             return False
-        #         # Likewise, avoid suppressing if a StopIteration exception
-        #         # was passed to throw() and later wrapped into a RuntimeError
-        #         # (see PEP 479).
-        #         if type is StopIteration and exc.__cause__ is value:
-        #             return False
-        #         raise
-        #     except:
-        #         # only re-raise if it's *not* the exception that was
-        #         # passed to throw(), because __exit__() must not raise
-        #         # an exception unless __exit__() itself failed.  But throw()
-        #         # has to raise the exception to signal propagation, so this
-        #         # fixes the impedance mismatch between the throw() protocol
-        #         # and the __exit__() protocol.
-        #         #
-        #         if sys.exc_info()[1] is value:
-        #             return False
-        #         raise
-        #     raise RuntimeError("generator didn't stop after throw()")
+        if type is None:
+            try:
+                await self.gen.__anext__()
+            except StopAsyncIteration:
+                return False
+            else:
+                raise RuntimeError("generator didn't stop")
+        else:
+            if value is None:
+                value = type()
+            try:
+                await self.gen.athrow(type, value, traceback)
+            except StopAsyncIteration as exc:
+                return exc is not value
+            except RuntimeError as exc:
+                if exc is value:
+                    return False
+                if type is StopAsyncIteration and exc.__cause__ is value:
+                    return False
+                raise
+            except:
+                if sys.exc_info()[1] is value:
+                    return False
+                raise
+            raise RuntimeError("generator didn't stop after throw()")
 
 
 def asynccontextmanager(func):
-    # FIXME doesn't execute code after yielding
     @wraps(func)
     def helper(*args, **kwds):
         return _AsyncGeneratorContextManager(func, args, kwds)
